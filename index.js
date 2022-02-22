43abb16508869be061c43ef16a983c90d4f5950a
luke <lukeledvina19@gmail.com>
2022-02-22 01:22:57 -0500

    Add functionality of displaying boxes with dynamic array size

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let myLibrary = [];
+let myLibrary = [123, 123, 123, 123, 123 ];
 
 function Book(author, pages, title) {
     this.author = author;
@@ -12,5 +12,20 @@ function addBookToLibrary() {
     let userPages = prompt("How many pages?");
 
     myLibrary.push(new Book(userAuthor, userPages, userTitle));
+    updateScreen(myLibrary);
 }
 
+let libraryContainer = document.getElementById("library-container");
+
+function updateScreen(library) {
+    for(let i = 0; i  < library.length; i++) {
+        libraryContainer.appendChild(document.createElement('div')).className = "card";
+
+        for(let key in library) {
+            //put book info on card
+
+        }
+    }
+}
+
+updateScreen(myLibrary);
