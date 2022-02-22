aacf565fda51645de987f119220d8954fe46e63c
luke <lukeledvina19@gmail.com>
2022-02-22 01:54:25 -0500

    Add add book button

--- index.js
+++ index.js
@@ -1,31 +1,47 @@
-let myLibrary = [123, 123, 123, 123, 123 ];
+let myLibrary = [];
 
-function Book(author, pages, title) {
+function Book(author, pages, title, read) {
     this.author = author;
     this.pages = pages;
     this.title = title;
+    this.read = read;
 }
 
-function addBookToLibrary() {
-    let userTitle = prompt("Title?");
-    let userAuthor = prompt("Author?");
-    let userPages = prompt("How many pages?");
+let userTitle;
+let userAuthor;
+let userPages;
+let userRead;
 
-    myLibrary.push(new Book(userAuthor, userPages, userTitle));
+function addBookToLibrary() {
+
+    // This function needs to create pop-up form with input fields for each
+
+    userTitle = prompt("Title?");
+    userAuthor = prompt("Author?");
+    userPages = prompt("How many pages?");
+    userRead = prompt("Have you read it?");
+
+    myLibrary.push(new Book(userAuthor, userPages, userTitle, userRead));
     updateScreen(myLibrary);
 }
 
 let libraryContainer = document.getElementById("library-container");
-
+let currentCard;
 function updateScreen(library) {
     for(let i = 0; i  < library.length; i++) {
-        libraryContainer.appendChild(document.createElement('div')).className = "card";
+        currentCard = libraryContainer.appendChild(document.createElement('div'));
+        currentCard.className = "card";
 
-        for(let key in library) {
+        for(let key in library[i]) {
             //put book info on card
-
+            console.log(`${key}: ${library[i][key]}`);
+            currentCard.textContent += `${key}: ${library[i][key]} \n`;
         }
     }
 }
 
-updateScreen(myLibrary);
+let addButton = document.getElementById("add-button");
+
+addButton.addEventListener("click", () => {
+    addBookToLibrary();
+})
